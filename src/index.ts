f66f588acb51a6fb3aaaa5c2f95fb63fdbc66581
dev-Shivam999 <shivamswami982@gmail.com>
2024-12-10 13:09:21 +0530

    cluster

--- src/index.ts
+++ src/index.ts
@@ -5,27 +5,48 @@ import { UserRouter } from './Routers/User';
 import cors from 'cors'
 import { SelRoute } from './Routers/Sel';
 import { config } from 'dotenv';
+
+import cluster from 'cluster';
+import os from 'os';
 config()
 
-const app = express();
-app.use(cors({
-    credentials:true,
-    origin:`${process.env.Url}`
-}))
-console.log(process.env.Url);
+const totalCpus = os.cpus().length;
+console.log(totalCpus);
 
 
-Db()
-app.use(express.json())
-app.use(cookieParser())
-app.use('/user', UserRouter)
-app.use('/Sel', SelRoute)
+if (cluster.isPrimary) {
+    console.log(`totalCpus: ${totalCpus}`);
+
+    for (let i = 0; i < totalCpus; i++) {
+        cluster.fork();
+    }
+
+    cluster.on("exit", (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} exited, starting a new one...`);
+        cluster.fork();
+    });
+}else{
+
+    const app = express();
+    app.use(cors({
+        credentials: true,
+        origin: `${process.env.Url}`
+    }))
+    console.log(process.env.Url);
 
 
-const Port = process.env.PORT||3000
+    Db()
+    app.use(express.json())
+    app.use(cookieParser())
+    app.use('/user', UserRouter)
+    app.use('/Sel', SelRoute)
 
 
-app.listen(Port,()=>{
-    console.log(`app listening on ${Port}`);
-    
-})
+    const Port = process.env.PORT || 3000
+
+
+    app.listen(Port, () => {
+        console.log(`app listening on ${Port}`);
+
+    })
+}
